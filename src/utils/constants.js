9185577fb12200fd715dc328ad48fe6c5b7718ce
robertorojas-cmd <roberto.rojas@urbani.com.mx>
2026-01-17 17:14:23 -0600

    constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 
 
 const remoteUrl = 'https://api-express-holy-cloud-5609.fly.dev/api/v1';
-const localUrl = ' http://localhost:3000/api/v1';
-const baseUrl = localUrl;
+const localUrl = 'http://localhost:3000/api/v1';
+const baseUrl = process.env.REACT_APP_BASE_URL || remoteUrl;
 
 export const BASE_URL = baseUrl;
